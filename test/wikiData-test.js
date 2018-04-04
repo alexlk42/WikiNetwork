2c864f2e147a9867f1e8c9beec356e19d6434ad9
Alex Killian <alex.killian@colorado.edu>
2018-04-04 15:21:55 -0600

    cleaned up WikiData class. made the private module functions static methods in WikiData

--- test/wikiData-test.js
+++ test/wikiData-test.js
@@ -3,14 +3,22 @@ const expect = require('chai').expect;
 const WikiData = require('../wikiData');
 
 describe('wikiData', () => {
-  it('should run callback', done => {
-    WikiData.getForwardLinks(['Computer_science'], 1, (result) => done());
+  it('should run callback when call returns', done => {
+    WikiData.callAPI(WikiData.createURLString('links', ['Computer_science']), (result) => done());
   });
 
   it('should return 5 links', done => {
     WikiData.getForwardLinks(['Computer_science'], 5, (result) => {
       expect(result).to.have.a.property('query');
       expect(result.query.pages[0].links.length).to.equal(5);
+      done();
+    });
+  });
+
+  it('should return results for 2 pages', done => {
+    WikiData.getForwardLinks(['Computer_science', 'IBM'], 1, (result) => {
+      expect(result).to.have.a.property('query');
+      expect(result.query.pages.length).to.equal(2);
       done();
     });
   });
--- wikiData.js
+++ wikiData.js
@@ -28,69 +28,69 @@ const FORMAT_VERSION = '2';
 const DEL = '|';
 
 /*
- * A helper function that creates the correct API url to do the wanted call.
- * This function is private to this module.
- */
-const createURLString = function (prop, titles) {
-  let tls = titles.join(DEL); // Join the titles
-  return `${API_URL}?` +
-        `action=${ACTION}` +
-        `&titles=${tls}` +
-        `&prop=${prop}` +
-        `&format=${FORMAT}` +
-        `&formatversion=${FORMAT_VERSION}`;
-};
-
-/*
- * A helper function that makes a call to the MediaWiki API.
- * This function is private to this module.
- */
-const callAPI = function (url, callback) {
-  if (!url) {
-    console.error('URL is not valid! Not doing API call.');
-    return;
-  }
-
-  if (!callback) {
-    console.error('No callback! No point in doing API call.');
-    return;
-  }
-
-  // Do an https GET to the given URL.
-  https.get(url, function (res) {
-    if (!res) {
-      console.error('No response!');
-      return;
-    }
-
-    // Set the output encoding
-    res.setEncoding('utf8');
-
-    // Collect result
-    let result = '';
-    res.on('data', function (data) {
-      result += data;
-    });
-
-    // Once result is fully here, we can parse and do callback
-    res.on('end', function () {
-      callback(JSON.parse(result));
-    });
-  });
-};
-
-/*
  * The actual WikiData class. All methods are static because they don't
  * need any state from an instance and are utility methods.
  */
 class WikiData {
 
   /*
+   * A helper function that creates the correct API url to do the wanted call.
+   */
+  static createURLString (prop, titles) {
+    let tls = titles.join(DEL); // Join the titles
+    return `${API_URL}?` +
+          `action=${ACTION}` +
+          `&titles=${tls}` +
+          `&prop=${prop}` +
+          `&format=${FORMAT}` +
+          `&formatversion=${FORMAT_VERSION}`;
+  }
+
+  /*
+   * A helper function that makes a call to the MediaWiki API. You may use
+   * this method as a stand-alone. But, you should use the high-level get methods
+   * in this class and try to only use this as a helper when possible.
+   */
+  static callAPI (url, callback) {
+    if (!url) {
+      console.error('URL is not valid! Not doing API call.');
+      return;
+    }
+
+    if (!callback) {
+      console.error('No callback! No point in doing API call.');
+      return;
+    }
+
+    // Do an https GET to the given URL.
+    https.get(url, function (res) {
+      if (!res) {
+        console.error('No response!');
+        return;
+      }
+
+      // Set the output encoding
+      res.setEncoding('utf8');
+
+      // Collect result
+      let result = '';
+      res.on('data', function (data) {
+        result += data;
+      });
+
+      // Once result is fully here, we can parse and do callback
+      res.on('end', function () {
+        callback(JSON.parse(result));
+      });
+    });
+  }
+
+  /*
    * Gets the forward links for the give page (specified by title). Will invoke
    * the given callback once results are ready. Passes the parsed JSON to the callback.
    */
-  static getForwardLinks(titles, limit, callback) {
-    callAPI(createURLString('links', titles) + `&pllimit=${limit};`, callback);
+  static getForwardLinks (titles, limit, callback) {
+    this.callAPI(this.createURLString('links', titles) + `&pllimit=${limit};`, callback);
   }
 }
 
