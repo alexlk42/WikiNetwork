9c551e848d7bf297f82459c240fc5a71c91b0d61
Kristopher Neilsen <kamaro67@gmail.com>
2018-04-05 19:46:28 -0600

    fixed index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ var express = require('express');
 var app = express();
 const WikiData = require('./wikiData');
 
-<<<<<<< HEAD
 app.get('/index.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
 })
@@ -35,16 +34,15 @@ app.get('/clear', function (req, res) {
    
 })
 
-app.get('/', function (req, res) {
-  res.send('Welcome to Wikinetworks');
+//app.get('/', function (req, res) {
+//  res.send('Welcome to Wikinetworks');
   //res.render('index', { title: 'Welcome to Wikinode', message: 'Wikinodes' });
-=======
+
 // Setup simple routing
 app.get('/', (req, res) => {
   WikiData.getCategories('Computer_science', 5, result => res.send(JSON.stringify(result)));
   //WikiData.getForwardLinks('Computer_science', 1, result => res.send(JSON.stringify(result)));
   //WikiData.getDescription('Computer_science', result => res.send(JSON.stringify(result)));
->>>>>>> develop
 });
 
 app.listen(3000, function () {
