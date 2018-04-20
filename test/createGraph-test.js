8b989e909a665f7684c15caff5c8295e0ff6e14b
Avery <adsc7378@colorado.edu>
2018-04-19 18:13:49 -0600

    createGraph test works again

--- test/createGraph-test.js
+++ test/createGraph-test.js
@@ -6,11 +6,9 @@ const genGraphJSON = require('../createGraph');
  * Unit test to ensure the correct graph is created
  */
 describe ('createGraph', () => {
-  it('should create an appropriate JSON file', done=>{
-    genGraphJSON(['computer_science', 'mathematics'], 5, 1, res=>{
-      expect(res.nodes.length).to.equal(11); //One node is shared by both
-      expect(res.links.length).to.equal(10); //Still 10 links in total
-      done();
-    });
+  it('should create an appropriate JSON file', async ()=>{
+    let res = await genGraphJSON(['computer_science', 'mathematics'], 5, 1);
+    expect(res.nodes.length).to.equal(11); //One node is shared by both
+    expect(res.links.length).to.equal(10); //Still 10 links in total
   });
 });
